f65bd6e8d3fb7adc83292b5308d1e22125ee2e93
ChairfaceChippendale <ujujzk@gmail.com>
2016-04-22 15:08:45 +0300

    second

--- app/src/main/java/com/gmail/tscybernetics/fenilevel/FeNiLevel.java
+++ app/src/main/java/com/gmail/tscybernetics/fenilevel/FeNiLevel.java
@@ -1,46 +1,82 @@
 package com.gmail.tscybernetics.fenilevel;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.SeekBar;
 import android.support.v7.widget.AppCompatSeekBar;
 import android.widget.TextView;
 
 public class FeNiLevel extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
-    private AppCompatSeekBar mNiInOreSeekBar;
-    private AppCompatSeekBar mChargeOreSeekBar;
+    private double bottomArea = .0;
+    private double feniDensity = .0;
+    private double niExtraction = .0;
 
-    private TextView mNiInOreTextView;
-    private TextView mChargeOreTextView;
+    private AppCompatSeekBar mFormerMetalLevelSeekBar;
+    private AppCompatSeekBar mChargeLoadedSeekBar;
+    private AppCompatSeekBar mNiInChargeSeekBar;
+    private AppCompatSeekBar mMetalMassSeekBar;
+    private AppCompatSeekBar mNiInMetalSeekBar;
+
+    private TextView mFormerMetalLevelTextView;
+    private TextView mNiInChargeTextView;
+    private TextView mChargeLoadedTextView;
+    private TextView mMetalMassTextView;
+    private TextView mNiInMetalTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feni_level);
 
-        mNiInOreSeekBar = (AppCompatSeekBar) findViewById(R.id.seekbar_ni_in_ore);
-        mChargeOreSeekBar = (AppCompatSeekBar) findViewById(R.id.seekbar_charge_ore);
+        mFormerMetalLevelSeekBar = (AppCompatSeekBar) findViewById(R.id.seekbar_former_metal_level);
+        mChargeLoadedSeekBar = (AppCompatSeekBar) findViewById(R.id.seekbar_charge_loaded);
+        mNiInChargeSeekBar = (AppCompatSeekBar) findViewById(R.id.seekbar_ni_in_charge);
+        mMetalMassSeekBar = (AppCompatSeekBar) findViewById(R.id.seekbar_metal_mass);
+        mNiInMetalSeekBar = (AppCompatSeekBar) findViewById(R.id.seekbar_ni_in_metal);
 
-        mNiInOreTextView = (TextView) findViewById(R.id.textview_ni_in_ore);
-        mChargeOreTextView = (TextView) findViewById(R.id.textview_charge_ore);
+        mFormerMetalLevelTextView = (TextView) findViewById(R.id.textview_former_metal_level);
+        mChargeLoadedTextView = (TextView) findViewById(R.id.textview_charge_loaded);
+        mNiInChargeTextView = (TextView) findViewById(R.id.textview_ni_in_charge);
+        mMetalMassTextView = (TextView) findViewById(R.id.textview_metal_mass);
+        mNiInMetalTextView  = (TextView) findViewById(R.id.textview_ni_in_metal);
 
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        mNiInOreSeekBar.setOnSeekBarChangeListener(this);
-        mChargeOreSeekBar.setOnSeekBarChangeListener(this);
+        mFormerMetalLevelSeekBar.setOnSeekBarChangeListener(this);
+        mChargeLoadedSeekBar.setOnSeekBarChangeListener(this);
+        mNiInChargeSeekBar.setOnSeekBarChangeListener(this);
+        mMetalMassSeekBar.setOnSeekBarChangeListener(this);
+        mNiInMetalSeekBar.setOnSeekBarChangeListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        bottomArea = sp.getFloat("bottom_area", 197.5f);
+        feniDensity = sp.getFloat("feni_density", 7.65f);
+        niExtraction = sp.getFloat("ni_extraction", 91.5f);
     }
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        if (seekBar == mNiInOreSeekBar) {
-            mNiInOreTextView.setText(String.valueOf(progress));
-        } else if (seekBar == mChargeOreSeekBar) {
-            mChargeOreTextView.setText(String.valueOf(progress));
+        if (seekBar == mFormerMetalLevelSeekBar) {
+            mFormerMetalLevelTextView.setText(String.valueOf(progress));
+        } else if (seekBar == mChargeLoadedSeekBar) {
+            mChargeLoadedTextView.setText(String.valueOf(progress));
+        } else if (seekBar == mNiInChargeSeekBar) {
+            mNiInChargeTextView.setText(String.valueOf(progress));
+        } else if (seekBar == mMetalMassSeekBar) {
+            mMetalMassTextView.setText(String.valueOf(progress));
+        } else if (seekBar == mNiInMetalSeekBar) {
+            mNiInMetalTextView.setText(String.valueOf(progress));
         }
     }
 
